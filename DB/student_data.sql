b1c951aeaf36f36fb6d20450fcfdd30ce75f817f
cmulay <codewithchin@gmail.com>
2021-06-16 19:28:30 +0530

    Update DB

--- DB/student_data.sql
+++ DB/student_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2021 at 04:12 PM
+-- Generation Time: Jun 16, 2021 at 03:57 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
