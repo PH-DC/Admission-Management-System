9e33dd9b91ec5d38464695efd4d79e75d0bd95e7
Dipen Gawand <88664051+Dipengawand@users.noreply.github.com>
2021-09-09 16:55:10 +0530

    Added Signuo.php

--- signup.php
+++ signup.php
@@ -1,37 +1,12 @@
 <?php
 
-$name_p = $_POST['name'];
-$email_p = $_POST['email'];
+$getuserInstitudeid=$_POST['institudeID'];
+$getuserFirstname=$_POST['firstName'];
+$getuserLastmame=$_POST['lastName'];
+$getuserEmail=$_POST['email'];
+$getuserDesignation=$_POST['designation'];
+$getuserPassword=$_POST['password'];
 
-$username_p = $_POST['username'];
-$password_p = $_POST['password'];
-
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="admission_system";
-
-$conn=new mysqli($servername,$username,$password,$dbname);
-
-if(isset($_POST['Signup']))
-{
-	if (!$conn) 
-	{
-		die('Could not connect'.mysqli_connect_error());
-	}
-	$sql = "INSERT INTO admission(Sname,Semail,Susername,Spassword) VALUES ('$name_p','$email_p','$username_p','$password_p')";
-
-	if($conn->query($sql) == TRUE)
-	{
-		echo "<script>";
-		echo "alert ('Your account was created successfully. Please login to continue.')";
-		echo "</script>";
-		include 'login.html';
-	}
-	else
-	{
-		include 'login.html';
-	}
-	$conn -> close();
-}
 ?>
+
+        
